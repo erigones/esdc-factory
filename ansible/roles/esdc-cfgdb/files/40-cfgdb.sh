7f01952a40c0c2682a0d99f0547be9eeb8c80d60
Daniel Kontšek <daniel@kontsek.sk>
2018-01-09 17:19:17 +0100

    Created ZooKeeper REST HTTP Service (#108)
    Issue #102
    The service will be automatically deployed on cfgdb only if an SSL key and certificate are both provided via metadata:
    * org.erigones:cfgdb_zk_rest_ssl_cert
    * org.erigones:cfgdb_zk_rest_ssl_key

--- ansible/roles/esdc-cfgdb/files/40-cfgdb.sh
+++ ansible/roles/esdc-cfgdb/files/40-cfgdb.sh
@@ -14,6 +14,9 @@ zkcli() {
 }
 
 CFGDB_INITIALIZED="/var/lib/cfgdb.initialized"
+ZK_REST_HTTP_SVC="svc:/application/cfgdb-zk-rest"
+ZK_REST_HTTP_SSL_CERT="/usr/local/etc/zk_rest_http_ssl.crt"
+ZK_REST_HTTP_SSL_KEY="/usr/local/etc/zk_rest_http_ssl.key"
 
 if [[ -f "${CFGDB_INITIALIZED}" ]]; then
     log "found ${CFGDB_INITIALIZED} - skipping cfgdb initialization"
@@ -39,6 +42,22 @@ if [[ -n "${CFGDB_USERNAME}" && -n "${CFGDB_PASSWORD}" ]]; then
 	zkcli "create ${CFGDB_NODE} ${CFGDB_DATA} digest:${CFGDB_AUTH}:crdwa"
 else
 	zkcli "create ${CFGDB_NODE} ${CFGDB_DATA}"
+fi
+
+ZK_REST_SSL_CERT="$(mdata-get org.erigones:cfgdb_zk_rest_ssl_cert 2>/dev/null || echo '')"
+log "metadata key org.erigones:cfgdb_zk_rest_ssl_cert value=${ZK_REST_SSL_CERT}"
+
+ZK_REST_SSL_KEY="$(mdata-get org.erigones:cfgdb_zk_rest_ssl_key 2>/dev/null || echo '')"
+log "metadata key org.erigones:cfgdb_zk_rest_ssl_key value=${ZK_REST_SSL_KEY}"
+
+svcadm disable -s "${ZK_REST_HTTP_SVC}"
+
+if [[ -n "${ZK_REST_SSL_CERT}" && -n "${ZK_REST_SSL_KEY}" ]]; then
+	echo "${ZK_REST_SSL_CERT}" > "${ZK_REST_HTTP_SSL_CERT}"
+	echo "${ZK_REST_SSL_KEY}" > "${ZK_REST_HTTP_SSL_KEY}"
+	chown nobody "${ZK_REST_HTTP_SSL_KEY}"
+	chmod 0400 "${ZK_REST_HTTP_SSL_KEY}"
+	svcadm enable -s "${ZK_REST_HTTP_SVC}"
 fi
 
 touch "${CFGDB_INITIALIZED}"
