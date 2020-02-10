0a3b93896f26c06f54c1bfaaf2fbf8aea6f82ea5
YanChii <janci@binaryparadise.com>
2020-02-10 21:08:11 +0000

    add pdns-confd service

--- ansible/roles/esdc-dns/files/41-dns.sh
+++ ansible/roles/esdc-dns/files/41-dns.sh
@@ -3,6 +3,7 @@
 MDATA_PREFIX="org.erigones"
 PDNS_CONFIG="/opt/local/etc/pdns.conf"
 PDNS_RECURSOR_CONFIG="/opt/local/etc/recursor.conf"
+PDNS_CONFD_CONFIG="/opt/local/etc/pdns-confd.ini"
 
 declare -A PDNS_MDATA=(
 	[pgsql_host]="gpgsql-host"
@@ -16,10 +17,19 @@ declare -A PDNS_RECURSOR_MDATA=(
 	[recursor_forwarders]="forward-zones-recurse"
 )
 
+declare -A PDNS_CONFD_MDATA=(
+	[pgsql_host]="@PGSQL_HOST@"
+	[pgsql_port]="@PGSQL_PORT@"
+	[pgsql_user]="@PGSQL_USER@"
+	[pgsql_password]="@PGSQL_PASSWORD@"
+	[pgsql_dbname]="@PGSQL_DBNAME@"
+)
+
 update_config() {
 	local mdata_key="${1}"
 	local config_var="${2}"
 	local config_file="${3}"
+	local comment_char="${4:-#}"
 	local mdata_value
 	local config_value
 
@@ -31,7 +41,7 @@ update_config() {
 		log "found metadata key: \"${mdata_key}\" value: \"${mdata_value}\""
 		if [[ -z "${mdata_value}" ]]; then
 			log "empty metadata value for key \"${mdata_key}\" -> commenting out \"${config_var}\"!"
-			config_value="#${config_var}="
+			config_value="${comment_char}${config_var}="
 		else
 			config_value="${config_var}=${mdata_value}"
 		fi
@@ -45,6 +55,22 @@ update_config() {
 	fi
 }
 
+replace_string_by_mdata() {
+	local mdata_key="${1}"
+	local old_string="${2}"
+	local config_file="${3}"
+	local mdata_value
+
+	log "reading metadata key: \"${mdata_key}\""
+	mdata_value=$(mdata-get "${mdata_key}" 2>/dev/null)
+
+	if gsed -i "s/${old_string}/${mdata_value}/g" "${config_file}"; then
+		log "set ${old_string} => ${mdata_value} in ${config_file}"
+	else
+		log "failed to set ${old_string} => ${mdata_value} in ${config_file}"
+	fi
+}
+
 log "reading pdns metadata and configuring ${PDNS_CONFIG}"
 for key in "${!PDNS_MDATA[@]}"; do
 	update_config "${MDATA_PREFIX}:${key}" "${PDNS_MDATA[$key]}" "${PDNS_CONFIG}"
@@ -55,6 +81,12 @@ for key in "${!PDNS_RECURSOR_MDATA[@]}"; do
 	update_config "${MDATA_PREFIX}:${key}" "${PDNS_RECURSOR_MDATA[$key]}" "${PDNS_RECURSOR_CONFIG}"
 done
 
-log "starting PowerDNS and PowerDNS recursor"
+log "reading pdns metadata and configuring ${PDNS_CONFD_MDATA}"
+for key in "${!PDNS_CONFD_MDATA[@]}"; do
+	replace_string_by_mdata "${MDATA_PREFIX}:${key}" "${PDNS_CONFD_MDATA[$key]}" "${PDNS_CONFD_CONFIG}"
+done
+
+log "starting PowerDNS services"
 svcadm enable -s pdns
 svcadm enable -s pdns-recursor
+svcadm enable -s pdns-confd
